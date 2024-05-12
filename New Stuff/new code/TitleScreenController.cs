bfd86c2ce88cd834f57ead395513a2b0a112d4f9
duydidathing <jason826@my.yorku.ca>
2024-05-12 17:52:26 -0400

    Add files via upload

---
+++ New Stuff/new code/TitleScreenController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TitleScreenController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public string destination;
+    void Start()
+    {
+        //SceneManager.LoadScene(destination);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("TutorialHall");
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void LoadControls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void QuitGame() 
+    { 
+        Application.Quit(); 
+    }
+
+}
